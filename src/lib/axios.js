9abbf5b10c5e0b38af97f6e88a980419c3fe6ce0
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-07-07 06:58:57 +0500

    add backend api

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.MODE == 'development' ? 'http://localhost:8080/api' : '/api'
+// const baseURL = import.meta.env.MODE == 'development' ? 'http://localhost:8080/api' : '/api'
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://chatty-backend-1pxd.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },

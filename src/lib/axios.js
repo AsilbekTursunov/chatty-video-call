c954f49dff9be8cb3c249ca23fe8f7b0ece691da
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-07-07 08:11:23 +0500

    change api

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const baseURL = import.meta.env.MODE == 'development' ? 'http://localhost:8080/api' : '/api'
 const axiosInstance = axios.create({
-  baseURL: 'https://chatty-backend-1pxd.onrender.com/api',
+  baseURL: 'https://chatty-backend-ughu.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },

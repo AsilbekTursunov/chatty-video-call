dbd333d3244bfd20964dcd9d69535f1a086b40ff
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-07-10 16:33:12 +0500

    change signin func

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -3,14 +3,17 @@ import { loginUser } from "../lib/api";
 
 export const useSignIn = () => {
   const queryClient = useQueryClient();
-  const { mutate: loginMutation, isPending, error } = useMutation({
+  const { mutate: loginMutation, isPending, error, data } = useMutation({
     mutationKey: ['login'],
     mutationFn: loginUser,
     retry: false, // Disable retry on failure
-    onSuccess: () => {
+    onSuccess: (data) => {
+      console.log('signin data', data);
       setTimeout(() => {
         queryClient.invalidateQueries(['authUser']);
       }, 3000)
+    }, onError(error) {
+      console.log('signin error', error);
     }
   });
   return { loginMutation, isPending, error };

cdb51296c8a6db894eac5f8628f80d350e143671
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-07-10 16:22:53 +0500

    change logout btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
 					/>
 				</CButton>
 				<CButton className='mx-2 '>
-					<LogOut onClick={logoutMutation} />
+					<LogOut onClick={() => logoutMutation()} />
 				</CButton>
 			</div>
 		</div>
